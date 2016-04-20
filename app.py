21ac3f1f2cdbc3ed7a3b8435dfea8b207230c489
Nikita Arinkin <nikita.arinkin@gmail.com>
2016-04-21 01:11:19 +0300

    First commit

--- app.py
+++ app.py
@@ -87,10 +87,9 @@ def handle(msg):
                 values = {'word' : msg["text"][3:],
                             'encoding' : "utf-8"}
                 url = "http://www.rlsnet.ru/search.htm"
-                data = urllib.parse.urlencode(values)
-                data = data.encode('utf-8') # data should be bytes
+ # data should be bytes
 
-                r = requests.get(url, data)
+                r = requests.get(url, values)
                 print(r.url)
                 parse_resp(r, msg)
 
