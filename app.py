3c230690d943d511a6c60c736afbe77aa7232b4b
Nikita Arinkin <nikita.arinkin@gmail.com>
2016-04-21 03:05:44 +0300

    First commit

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ except ImportError:
 def parse_resp(resp, content, msg):
     global gettingnum
     if resp.status == 200:
-        the_page = unicode(content, 'cp1251')
+        the_page = content.decode("utf-8", "ignore")
         print(the_page)
         soup = BeautifulSoup(the_page, 'html.parser')
         dv = False
